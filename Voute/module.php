02571eeecf5f4c4a149e2164c122ee2b13288e9f
Martin Kleinhans <kleinhans.martin@bcg.com>
2022-05-18 20:30:39 +0200

    initial commit

---
+++ Voute/module.php
@@ -0,0 +1,58 @@
+<?php
+
+class ESPDevice extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+        // properties
+        $this->RegisterPropertyString('Topic', '');
+
+        // variables
+        $this->RegisterVariableBoolean("Connected", "Connected");
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        $topic = $this->ReadPropertyString('Topic');
+        $this->SetReceiveDataFilter('.*' . $topic . '.*');
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        $this->SendDebug('JSON', $JSONString, 0);
+        if (empty($this->ReadPropertyString('Topic'))) return;
+
+        $data = json_decode($JSONString);
+
+        $Buffer = $data;
+
+        $topic = $this->ReadPropertyString('Topic');
+        $lwtTopic = $this->ReadPropertyString('LastWillTopic');
+        $statusTopic = $this->ReadPropertyString('StatusTopic');
+
+        if (fnmatch('*/online', $Buffer->Topic)) {
+            $this->SetValue("Connected", $Buffer->Payload === 'Online' ? true : false);
+        }
+    }
+
+    public function SendRequest($Ident, $Value)
+    {
+        //MQTT Server
+        $Server['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Server['PacketType'] = 3;
+        $Server['QualityOfService'] = 0;
+        $Server['Retain'] = false;
+        $Server['Topic'] = 'shellies/' . $this->ReadPropertyString('Topic') . '/' . $Ident;
+        $Server['Payload'] = $Value;
+        $ServerJSON = json_encode($Server, JSON_UNESCAPED_SLASHES);
+        $resultServer = $this->SendDataToParent($ServerJSON);
+    }
+}
