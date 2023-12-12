00808c47a683a17159bd096084df4c8b4bde3552
kleieg <egbert.kleinhans@gmail.com>
2023-12-12 16:59:33 +0100

    Batteri % -> Volt

--- ShellyBluRuuvi/module.php
+++ ShellyBluRuuvi/module.php
@@ -15,7 +15,7 @@ class ShellyBluRuuvi extends IPSModule
         // variables
         $this->RegisterVariableFloat("Temperature", "Temperature", "~Temperature");
         $this->RegisterVariableFloat("Humidity", "Humidity", "~Humidity.F");
-        $this->RegisterVariableInteger("Battery", "Battery", '~Battery.100');
+        $this->RegisterVariableInteger("Battery", "Battery", '~Volt');
 
     }
 
